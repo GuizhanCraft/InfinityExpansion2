4e0c534e32a5eb824b3935a0b4407960ae4ca561
ybw0014 <i@guizhanss.com>
2025-03-27 23:08:27 -0700

    fix(SingularityConstructor): fix block break, progress increase, add hud handler

--- src/main/kotlin/net/guizhanss/infinityexpansion2/implementation/items/machines/SingularityConstructor.kt
+++ src/main/kotlin/net/guizhanss/infinityexpansion2/implementation/items/machines/SingularityConstructor.kt
@@ -1,5 +1,7 @@
 package net.guizhanss.infinityexpansion2.implementation.items.machines
 
+import io.github.schntgaispock.slimehud.util.HudBuilder
+import io.github.schntgaispock.slimehud.waila.HudRequest
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType
@@ -7,13 +9,16 @@ import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils
 import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu
 import net.guizhanss.guizhanlib.kt.minecraft.extensions.dropItem
+import net.guizhanss.infinityexpansion2.InfinityExpansion2
 import net.guizhanss.infinityexpansion2.core.IERegistry
+import net.guizhanss.infinityexpansion2.core.items.annotations.HudProvider
 import net.guizhanss.infinityexpansion2.core.items.attributes.InformationalRecipeDisplayItem
 import net.guizhanss.infinityexpansion2.core.menu.MenuLayout
 import net.guizhanss.infinityexpansion2.implementation.items.machines.abstracts.AbstractTickingMachine
 import net.guizhanss.infinityexpansion2.implementation.items.materials.Singularity
 import net.guizhanss.infinityexpansion2.utils.items.GuiItems
 import net.guizhanss.infinityexpansion2.utils.items.toDisplayItem
+import net.guizhanss.infinityexpansion2.utils.slimefunext.getBlockMenu
 import net.guizhanss.infinityexpansion2.utils.slimefunext.getInt
 import net.guizhanss.infinityexpansion2.utils.slimefunext.getString
 import net.guizhanss.infinityexpansion2.utils.slimefunext.setInt
@@ -22,6 +27,7 @@ import org.bukkit.block.Block
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.inventory.ItemStack
 
+@HudProvider
 class SingularityConstructor(
     itemGroup: ItemGroup,
     itemStack: SlimefunItemStack,
@@ -38,7 +44,7 @@ class SingularityConstructor(
         val progress = menu.getProgress()
         val target = menu.getTarget()
         if (progress > 0 && target != null) {
-            val singularity = getSingularity(target)
+            val singularity = getSingularityById(target)
             if (singularity != null) {
                 val entry = singularity.ingredients.entries.first { it.value == 1 }
                 entry.key.dropItem(l, progress)
@@ -58,7 +64,7 @@ class SingularityConstructor(
     override fun process(b: Block, menu: BlockMenu): Boolean {
         val input = menu.getItemInSlot(inputSlots[0])
         val inputTemplate = input?.clone()?.apply { amount = 1 }
-        val singularity = getSingularity(menu.getTarget()) ?: return menu.checkNewSingularity(inputTemplate)
+        val singularity = getSingularityById(menu.getTarget()) ?: return menu.checkNewSingularity(inputTemplate)
 
         // target is valid
         var progress = menu.getProgress()
@@ -79,13 +85,15 @@ class SingularityConstructor(
             menu.pushItem(singularity.item.clone(), *outputSlots)
         } else {
             // check input and add progress
-            if (singularity != getTargetSingularity(inputTemplate)) {
+            if (singularity != getSingularityByIngredient(inputTemplate)) {
                 menu.setStatus { GuiItems.INVALID_INPUT }
                 return false
             }
 
-            menu.consumeItem(inputSlots[0], speed)
-            progress += singularity.getIngredientProgress(inputTemplate) * speed
+            // calculate the amount of items to consume
+            val amount = input.amount.coerceAtMost(speed)
+            menu.consumeItem(inputSlots[0], amount)
+            progress += singularity.getIngredientProgress(inputTemplate) * amount
         }
 
         menu.setProgress(progress)
@@ -95,7 +103,7 @@ class SingularityConstructor(
     }
 
     private fun BlockMenu.checkNewSingularity(input: ItemStack?): Boolean {
-        val singularity = getTargetSingularity(input) ?: run {
+        val singularity = getSingularityByIngredient(input) ?: run {
             setStatus { GuiItems.INVALID_INPUT }
             return false
         }
@@ -111,18 +119,10 @@ class SingularityConstructor(
         return true
     }
 
-    private fun BlockMenu.getTarget() = location.getString(TARGET)
-
-    private fun BlockMenu.setTarget(target: String) = location.setString(TARGET, target)
-
-    private fun BlockMenu.getProgress() = location.getInt(PROGRESS)
-
-    private fun BlockMenu.setProgress(progress: Int) = location.setInt(PROGRESS, progress)
-
     private fun BlockMenu.updateProgressDisplay() {
         if (!hasViewer()) return
         val progress = getProgress()
-        val singularity = getSingularity(getTarget())
+        val singularity = getSingularityById(getTarget())
         if (progress == 0 || singularity == null) {
             replaceExistingItem(PROGRESS_SLOT, ChestMenuUtils.getBackground())
             replaceExistingItem(TARGET_ITEM_DISPLAY_SLOT, ChestMenuUtils.getBackground())
@@ -130,17 +130,6 @@ class SingularityConstructor(
             replaceExistingItem(PROGRESS_SLOT, GuiItems.progressBar(progress, singularity.totalProgress))
             replaceExistingItem(TARGET_ITEM_DISPLAY_SLOT, singularity.item.toDisplayItem())
         }
-    }
-
-    private fun getTargetSingularity(item: ItemStack?): Singularity? {
-        if (item == null) return null
-        return IERegistry.singularityIngredientMap.entries.firstOrNull { (ingredient, _) ->
-            SlimefunUtils.isItemSimilar(item, ingredient, false)
-        }?.value
-    }
-
-    private fun getSingularity(id: String?): Singularity? {
-        return IERegistry.singularities.find { it.id == id }
     }
 
     override fun getInformationalItems() = listOf(
@@ -157,5 +146,56 @@ class SingularityConstructor(
         // gui slots
         private const val PROGRESS_SLOT = 3
         private const val TARGET_ITEM_DISPLAY_SLOT = 5
+
+        private fun BlockMenu.getTarget() = location.getString(TARGET)
+        private fun BlockMenu.setTarget(target: String) = location.setString(TARGET, target)
+
+        private fun BlockMenu.getProgress() = location.getInt(PROGRESS)
+        private fun BlockMenu.setProgress(progress: Int) = location.setInt(PROGRESS, progress)
+
+        private fun getSingularityByIngredient(item: ItemStack?): Singularity? {
+            if (item == null) return null
+            return IERegistry.singularityIngredientMap.entries.firstOrNull { (ingredient, _) ->
+                SlimefunUtils.isItemSimilar(item, ingredient, false)
+            }?.value
+        }
+
+        private fun getSingularityById(id: String?): Singularity? {
+            return IERegistry.singularities.find { it.id == id }
+        }
+
+        @Suppress("unused")
+        fun hudHandler(request: HudRequest): String {
+            val loc = request.location
+            val menu = loc.getBlockMenu() ?: return ""
+            val machine = request.slimefunItem as SingularityConstructor
+
+            val progress = menu.getProgress()
+
+            return buildString {
+                // default energy
+                append(HudBuilder.formatEnergyStored(machine.getCharge(loc), machine.capacity))
+
+                if (progress > 0) {
+                    val singularity = getSingularityById(menu.getTarget()) ?: return@buildString
+
+                    append("&7 | ")
+
+                    // progress
+                    val progress = menu.getProgress()
+                    val totalProgress = singularity.totalProgress
+                    append("$progress / $totalProgress")
+
+                    // target singularity
+                    append("&7 | ")
+                    append(
+                        InfinityExpansion2.integrationService.getTranslatedItemName(
+                            request.player,
+                            singularity.item
+                        )
+                    )
+                }
+            }
+        }
     }
 }
