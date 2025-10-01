6314990577aaf047a7e5bb75a677745a41215488
ybw0014 <i@guizhanss.com>
2025-09-30 20:09:51 -0400

    chore: update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 plugins {
-    kotlin("jvm") version "2.1.21"
+    kotlin("jvm") version "2.2.20"
     id("com.gradleup.shadow") version "9.2.2"
-    id("de.eldoria.plugin-yml.bukkit") version "0.7.1"
-    id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("de.eldoria.plugin-yml.bukkit") version "0.8.0"
+    id("xyz.jpenilla.run-paper") version "3.0.0"
 }
 
 group = "net.guizhanss"
